d919107fce4a13d404227c49e2cc1331bd4303f6
Mykyta Demeshchenko <mdemeshchenko@sdl.com>
2016-01-31 23:24:45 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Rails mongoid.yml
+
+This [wercker step](http://devcenter.wercker.com/docs/steps) generates a `config/mongoid.yml` rails project using [mongoid](http://mongoid.org).
+
+[![wercker status](https://app.wercker.com/status/0f970597f3b22cab6008a3ab598114e3/s/master "wercker status")](https://app.wercker.com/project/bykey/0f970597f3b22cab6008a3ab598114e3)
+
+## Example
+
+```yml
+box: ruby
+services:
+  - mongo
+build:
+  steps:
+    - rails-mongoid5-yml:
+        db_name: my_app_test
+```
+
+## Options
+
+- `db_name`
+  (optinal, default: `mongo`)
+  This option is not required. If set, it will name the database accordingly.
+
+## Details
+
+You should use this along with the mongo service.
+Have a look at the [services documentation](http://devcenter.wercker.com/docs/services) for more information.
+
+## License
+
+MIT License (MIT)
---
+++ run.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+main() {
+  local mongoid_yml_path="$PWD/config/mongoid.yml"
+
+  if [ -f "$mongoid_yml_path" ]; then
+    debug 'config/mongoid.yml already exists and will be overwritten'
+  fi
+
+  if [ -z "$MONGO_PORT_27017_TCP_ADDR" ]; then
+    warn "MONGO_PORT_27017_TCP_ADDR env var for the mongo service is not set"
+    return
+  fi
+
+  if [ -z "$MONGO_PORT_27017_TCP_PORT" ]; then
+    warn "MONGO_PORT_27017_TCP_ADDR env var for the mongo service is not set"
+    return
+  fi
+
+  if [ -z "$WERCKER_RAILS_MONGOID_YML_DB_NAME" ]; then
+    local db_name_default='mongoid'
+    debug "option db_name not set, defaulting to $db_name_default"
+    export WERCKER_RAILS_MONGOID_YML_DB_NAME="$db_name_default"
+  fi
+
+  info "exporting MONGOID_ENV='test' as a fallback"
+  export MONGOID_ENV='test'
+  info "Generating mongoid.yml template"
+  mkdir -p "$(dirname "${mongoid_yml_path}")"
+  tee "$mongoid_yml_path" << EOF
+test:
+  clients:
+    default:
+      database: <%= ENV['WERCKER_RAILS_MONGOID_YML_DB_NAME'] %>
+      hosts:
+        - <%= ENV['MONGO_PORT_27017_TCP_ADDR'] %>:<%= ENV['MONGO_PORT_27017_TCP_PORT'] %>
+      options:
+        read:
+          mode: :primary
+        max_retries: 1
+        retry_interval: 0
+EOF
+}
+
+main;
